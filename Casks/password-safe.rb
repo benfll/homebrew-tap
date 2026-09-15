8461cbb4af2435d5948e8fee70d1d07135fb7471
Benjamin Lambert <36955951+benfll@users.noreply.github.com>
2026-09-15 18:28:47 +0000

    password-safe 1.25.0

--- Casks/password-safe.rb
+++ Casks/password-safe.rb
@@ -1,6 +1,6 @@
 cask "password-safe" do
-  version "1.23.0"
-  sha256 "5d95740a1a5cede8b11a858d173feed56a2dd20349b97a96cc1f47a5c0d1e41e"
+  version "1.25.0"
+  sha256 "a0165c9e4a8fbefb449c97a450bf57dde1fc12682d3d5d895335d9050c2e3588"
 
   url "https://downloads.sourceforge.net/passwordsafe/PasswordSafe-macOS-#{version}-universal.dmg"
   name "pwsafe"
